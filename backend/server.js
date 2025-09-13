45663a4af98fddc3593d782b2beb3259319f5294
Sourish Ghosh <sghosh.ile.7@gmail.com>
2025-09-13 23:09:22 +0530

    Bug Fix: Build error fix

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// Handle preflight requests
-app.options('*', cors(corsOptions));
-
 // Body parsing middleware
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
